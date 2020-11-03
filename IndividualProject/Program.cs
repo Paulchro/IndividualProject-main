49dd162cb3276bdfbf1a7738301549c02da2479b
Paul Chronakis <paulchro@hotmail.com>
2020-11-03 16:06:36 +0200

    Add project files.

---
+++ IndividualProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IndividualProject.Models;
+using IndividualProject.BusinessLogic;
+
+namespace IndividualProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandPromptUtils cpUtils = new CommandPromptUtils();
+            List<Trainer> trainers = new List<Trainer>();
+            trainers.Add(cpUtils.GetTrainerDetails());
+            cpUtils.PrintTrainersList(trainers);
+
+        }
+
+        // Create appropriate methods in order to ask 
+        // from the user data for the four main classes
+    }
+}
